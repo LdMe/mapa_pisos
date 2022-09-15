8f55aba7b832922f3af777d10c65dd4e51e5191f
danel-torre-ubuntu <dlafuente003@gmail.com>
2022-09-15 19:59:57 +0200

    run script modified

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-docker run  -d  --mount type=bind,source=/home/danel/Python/Python3/mapa_pisos/,target=/app/ mapa_pisos
+docker run  -d  --mount type=bind,source=$PWD,target=/app/ mapa_pisos
